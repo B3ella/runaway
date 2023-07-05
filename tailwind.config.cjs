82488d5c8e6e6b09e3a50a348dfdc96df7a20f95
Isabella Oliveira <goliveirapk@gmail.com>
2023-07-05 10:18:23 -0300

    feat: content and style of main page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				"woman-runnig": "url('/woman-running.jpg')",
+			},
+			animation: {
+				blink: "blink 1s step-start infinite",
+			},
+			keyframes: {
+				blink: {
+					"0%, 100%": { opacity: 1 },
+					"50%": { opacity: 0 },
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
