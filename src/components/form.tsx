c556f9be42c780262bef27e24268e2408a543304
Isabella Oliveira <goliveirapk@gmail.com>
2023-07-07 09:37:38 -0300

    feat: saves runs data to local storage

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,32 +1,66 @@
 import { useState } from "react";
 
+interface run {
+	name: string;
+	distance: number;
+	time: number;
+}
+
+const runTolken = "run";
+
+function getRuns(): run[] {
+	const runs = localStorage.getItem(runTolken) ?? "[]";
+	return JSON.parse(runs);
+}
+
+function setRuns(runs: run[]): void {
+	const sRuns = JSON.stringify(runs);
+	localStorage.setItem(runTolken, sRuns);
+}
+
 export default function Form() {
-    const [distance, setDistance] = useState(0)
-    const [time, setTime] = useState(0)
-    const [runName, setRunName] = useState("")
+	const [distance, setDistance] = useState(0);
+	const [time, setTime] = useState(0);
+	const [runName, setRunName] = useState("");
+
+	const runs = getRuns();
 
 	function saveRun(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
 		event.preventDefault();
-        const runData = JSON.stringify({distance, time})
+		const runData = { distance, time, name: runName };
 
-		localStorage.setItem(runName, runData )
-        const data = localStorage.getItem(runName)
-        if( data) console.log(JSON.parse(data))
+		runs.push(runData);
+		setRuns(runs);
 	}
 
 	return (
 		<form className="flex flex-col gap-4 items-baseline w-fit m-auto">
 			<label>
 				Distance (Km)
-				<input id="distance" type="number" value={distance} onChange={(e)=>setDistance(+e.currentTarget.value)} />
+				<input
+					id="distance"
+					type="number"
+					value={distance}
+					onChange={(e) => setDistance(+e.currentTarget.value)}
+				/>
 			</label>
 			<label>
 				Time (minutes)
-				<input id="time" type="number" value={time} onChange={(e)=>setTime(+e.currentTarget.value)}/>
+				<input
+					id="time"
+					type="number"
+					value={time}
+					onChange={(e) => setTime(+e.currentTarget.value)}
+				/>
 			</label>
 			<label>
 				Run name (optinal)
-				<input id="run-name" type="text" value={runName} onChange={(e)=>setRunName(e.currentTarget.value)}/>
+				<input
+					id="run-name"
+					type="text"
+					value={runName}
+					onChange={(e) => setRunName(e.currentTarget.value)}
+				/>
 			</label>
 			<button onClick={saveRun}>save run</button>
 		</form>
