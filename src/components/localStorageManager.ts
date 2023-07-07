d914f797d36f3511429a3d21a36bea5eadda44d3
Isabella Oliveira <goliveirapk@gmail.com>
2023-07-07 11:14:19 -0300

    refactor: find run index own its own func

--- src/components/localStorageManager.ts
+++ src/components/localStorageManager.ts
@@ -24,23 +24,30 @@ function addRun(run: run): run[] {
 	return newRuns;
 }
 
+function areTheSameRun(run1: run, run2: run): boolean {
+	const sameDistance = run1.distance === run2.distance;
+	const sametime = run1.time === run2.time;
+	const sameName = run1.name === run2.name;
+	const sameRun = sameDistance && sametime && sameName;
+	return sameRun;
+}
+
+function findRunIndex(targetRun: run): number {
+	const currRuns = getRuns();
+	return currRuns.findIndex((run) => areTheSameRun(run, targetRun));
+}
+
 function removeRun(targetRun: run): run[] {
 	const currRuns = getRuns();
 
-	const index = currRuns.findIndex((run) => {
-		const sameDistance = run.distance === targetRun.distance;
-		const sametime = run.time === targetRun.time;
-		const sameName = run.name === targetRun.name;
-		const sameRun = sameDistance && sametime && sameName;
-		return sameRun;
-	});
+	const index = findRunIndex(targetRun);
 
 	const runDoesNotExist = index < 0;
 
 	if (runDoesNotExist) return currRuns;
 
-	const shouldDeleteAllRuns = index === 0;
-	if (shouldDeleteAllRuns) {
+	const isLastRun = index === 0;
+	if (isLastRun) {
 		setRuns([]);
 		return [];
 	}
