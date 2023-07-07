95f8129a899cc694c8a08a14c99660e99a399bb1
Isabella Oliveira <goliveirapk@gmail.com>
2023-07-07 10:32:13 -0300

    prettier

--- src/components/formAndVisualizer.tsx
+++ src/components/formAndVisualizer.tsx
@@ -52,7 +52,7 @@ function Form({ addRun }: { addRun: (arg: run) => void }) {
 function RunLi({ name, distance, time }: run) {
 	const meanVelocity = (distance / time) * 60;
 	const key = `${name}${time}${meanVelocity}`;
-    
+
 	return (
 		<li className="flex justify-between w-1/3" key={key}>
 			<h3>{name}</h3>
