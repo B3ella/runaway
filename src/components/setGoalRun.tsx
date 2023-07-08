6ad25d3616354ef1855588d334a79b4f405cdb12
Isabella Oliveira <goliveirapk@gmail.com>
2023-07-08 13:10:21 -0300

    feat: functional set goal run

--- src/components/setGoalRun.tsx
+++ src/components/setGoalRun.tsx
@@ -1,8 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getGoalRun, setGoalRun } from "./localStorageManager";
 
 export default function SetGoal() {
 	const [distance, setDistance] = useState(0);
 	const [time, setTime] = useState(0);
+
+	const defaultRunState = { time: 0, distance: 0 };
+	const [goalState, setGoalState] = useState(defaultRunState);
+
+	useEffect(() => {
+		setGoalState(getGoalRun() ?? defaultRunState);
+	}, []);
+
+	type changeEvent = React.ChangeEvent<HTMLInputElement>;
+	const changeDistance = (e: changeEvent) => setDistance(+e.target.value);
+	const changeTime = (e: changeEvent) => setTime(+e.target.value);
+
+	function resetValues() {
+		setDistance(0);
+		setTime(0);
+	}
+
+	function setGoal(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+		event.preventDefault();
+
+		const run = { distance, time };
+
+		setGoalRun(run);
+		setGoalState(run);
+
+		resetValues();
+	}
 
 	return (
 		<section className="h-screen">
@@ -12,15 +40,21 @@ export default function SetGoal() {
 			<form className="m-auto w-fit gap-4 flex flex-col">
 				<label>
 					Distance (Km)
-					<input type="number" />
+					<input
+						type="number"
+						value={distance}
+						onChange={changeDistance}
+					/>
 				</label>
 				<label>
 					Time (minutes)
-					<input type="number" />
+					<input type="number" value={time} onChange={changeTime} />
 				</label>
+				<button onClick={setGoal}>set goal run</button>
 			</form>
 			<p className="m-auto w-fit p-16">
-				Current goal: {"x"} Kms in {"y"} minutes
+				Current goal: {goalState.distance} Kms in {goalState.time}{" "}
+				minutes
 			</p>
 		</section>
 	);
